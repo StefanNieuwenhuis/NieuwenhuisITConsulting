0a9a3ec0f67f30a89d56be8e9ba5eb368d21d358
Stefan Nieuwenhuis <stefannhs@gmail.com>
2020-09-09 13:46:55 +0200

    Update GA settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-96319336-2',
-        head: false,
+        head: true,
       },
     },
   ],
